217bec8364f61b946274b962cc26c0d89ff46693
Hamza EL Yousfi <visioncomputer200@gmail.com>
2022-07-14 11:51:47 +0200

    Add files via upload

---
+++ array_adt.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+struct Array
+{
+    int *A;
+    int size;
+};
+
+void Display(Array arr)
+{
+    std::cout << "The elements are :" << std::endl;
+    for(int i=0; i<arr.size; i++){
+        std::cout << arr.A[i] << " ";
+    }
+}
+
+
+int main()
+{
+    Array arr;
+    std::cout << "Enter size : " << std::endl;
+    std::cin >> arr.size;
+    arr.A = new int[arr.size];
+
+    std::cout << "Enter all elements : " << std::endl;
+    for(int i=0; i<arr.size; i++){
+        std::cin >> arr.A[i];
+    }
+
+    Display(arr);
+    return 0;
+}
